451ccc586c8e440e535b97b4c9e59a8f85a1a3ea
Jonathan <jtrinh1@umbc.edu>
2025-06-19 00:39:44 -0400

    Added the ability to track entry time #16

--- src/Vehicles/Vehicle.java
+++ src/Vehicles/Vehicle.java
@@ -2,15 +2,19 @@ package Vehicles;
 
 import Billable.Billable;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public abstract class Vehicle implements Billable {
 
     private String licensePlate;
     private String state;
+    private LocalDateTime entryTime;
 
     public Vehicle(String licensePlate, String state) {
         this.licensePlate = licensePlate;
         this.state = state;
+        this.entryTime = LocalDateTime.now();
     }
 
     public String getLicensePlate() {
@@ -21,6 +25,24 @@ public abstract class Vehicle implements Billable {
         return state;
     }
 
+    public LocalDateTime getEntryTime() {
+        return entryTime;
+    }
 
+    public long getHoursParked() {
+        return Duration.between(entryTime, LocalDateTime.now()).toHours() + 1;
+    }
+
+    public abstract double getRatePerHour();
+
+    @Override
+    public double calculateFee() {
+        return getHoursParked() * getRatePerHour();
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + " [licensePlate=" + licensePlate + ", state=" + state + ", entryTime=" + entryTime + "]";
+    }
 
 }
